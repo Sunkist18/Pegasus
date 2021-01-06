1d82969ea974fee1d1e777b03c727f7e18927846
Dandalf <dandalf@o.cnu.ac.kr>
2021-01-06 15:03:35 +0900

    [*] VERSION TEST

--- web.py
+++ web.py
@@ -4,6 +4,10 @@ from pprint import pprint
 import requests
 from bs4 import BeautifulSoup
 
+# OPTIONS
+VERSION = '1.0.0'
+HIDE_IP = False
+
 
 def error(msg, place, kill=True):
     print('[*] 치명적인 오류가 발생했습니다')
@@ -31,7 +35,7 @@ def getDataFromLogin(userId, userPw):
         'user_id': userId,
         'user_password': userPw,
         'is_local': 'Y',
-        'user_ip': str(socket.gethostbyname(socket.getfqdn())),
+        'user_ip': '' if HIDE_IP else str(socket.gethostbyname(socket.getfqdn())),
         'sub_group_cd': ''
     }
 
@@ -68,11 +72,24 @@ def getLecture(data):
     return getDataFromVideoList(lecture_soup.find_all(class_='ag_c pd_ln'))
 
 
+def getNotice(data):
+    notice_page = 'http://e-learning.cnu.ac.kr/lms/class/boardItem/doListView.dunet'
+    data['mnid'], data['board_no'] = '201008945595', '7'
+    res = session.post(url=notice_page, data=data)
+    res.raise_for_status()
+    notice_soup = BeautifulSoup(res.text, 'html.parser')
+    return list(map(lambda x: ' '.join(x.text.split()), notice_soup.find_all('a', {'name': 'btn_board_view'})))
+
+
 def data_process(soup):
     database_ = {}
-    for class_ in soup.find_all('a', class_='classin2'):
+    index = 0
+    classes = soup.find_all('a', class_='classin2')
+    for class_ in classes:
+        index += 1
         class_page = 'http://e-learning.cnu.ac.kr/lms/class/classroom/doViewClassRoom_new.dunet'
         class_name, class_id, class_no, data = getDataFromCourse(class_)
+        print(f'[*] {class_name} [{index}/{len(classes)}]')
         # subject 기록
         database_[class_name] = {'no': class_no, 'id': class_id}
 
@@ -81,14 +98,7 @@ def data_process(soup):
 
         # 강의수강
         database_[class_name]['lecture'] = getLecture(data)
-
-        # # TODO REMOVE THIS
-
-        data['mnid'] = '201008945595'
-        res = session.post(data=data)
-        # # TODO END
-
-        database_[class_name]['notice'] = None  # TODO 과목공지 만들 것
+        database_[class_name]['notice'] = getNotice(data)
         database_[class_name]['assignment'] = None  # TODO 과제제출 만들 것
     return database_
 
@@ -98,7 +108,7 @@ if __name__ == '__main__':
     session = requests.session()
 
     # Login Process
-    user_id, user_pw = '202002570', '010107'
+    user_id, user_pw = '202002488', '010618'
     main_soup = login_process(user_id, user_pw)
 
     database = data_process(main_soup)
